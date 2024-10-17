2a9112b4c5571ebbe076d1a897fc0359f6e9b329
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2024-10-16 23:40:11 -0400

    Adicione archivos_pdf que seleccionar si es private o public cuando se quiere eliminar el articulo que elimine el archivo tambien

--- tm_app/tm_crm/doctype/archivos_pdf/archivos_pdf.py
+++ tm_app/tm_crm/doctype/archivos_pdf/archivos_pdf.py
@@ -1,9 +1,31 @@
 # Copyright (c) 2024, Niurka and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import os
 
 
 class ArchivosPDF(Document):
 	pass
+
+
+@frappe.whitelist()
+def eliminar_archivo_pdf(archivo_id):
+    # Obteniendo el archivo pdf
+    archivo_doc = frappe.get_doc('Archivos PDF', archivo_id)
+    
+    # Preguntando si es privado o public para eliminar segun la ruta
+    if archivo_doc.is_private:
+        ruta_artivo = '/sites/tecnomatica.local/private/files/' + archivo_doc.archivo_pdf
+    else:
+        ruta_artivo = '/sites/tecnomatica.local/public/files/' + archivo_doc.archivo_pdf
+    
+    # Verificando si el archivo existe antes de intentar elimiinarlo
+    if os.path.exists(ruta_artivo):
+        os.remove(ruta_artivo)
+    
+    # Eliminar el documento del doctype
+    archivo_doc.delete()
+    
+    return True
--- tm_app/tm_crm/doctype/archivos_pdf/archivos_pdf_list.js
+++ tm_app/tm_crm/doctype/archivos_pdf/archivos_pdf_list.js
@@ -1,16 +1,41 @@
 frappe.listview_settings['Archivos PDF'] = {
     on_page_load: function(listview) {
-        //Agregando evento al hacer click en el nombre del archivo
-        listview.on('click','.file-name', function() {
-            //Obteniendo la ruta del archivo desde el atributo data-ruta
-            var ruta_archivo = $(this).data('ruta');
-            //Verificar ruta no este vacia
-            if(ruta_archivo) {
-                //Abrir pdf en una ventana
-                window.open(ruta_archivo, '_blank');
-            }else{
-                frappe.msgprint(__('La ruta del archivo no existe'));
+        // Agregando evento al hacer clic en el nombre del archivo
+        listview.on("click", ".file-name", function() {
+            var ruta_archivo = $(this).data("ruta");
+            if (ruta_archivo) {
+                window.open(ruta_archivo, "_blank");
+            } else {
+                frappe.msgprint(__("La ruta del archivo no existe"));
+            }
+        });
+
+        // Agregar evento al cambiar el estado de selección
+        listview.on('check', function() {
+            var selected = listview.get_checked_items();
+            if (selected.length > 0) {
+                // Confirmar eliminación
+                frappe.confirm(
+                    __('¿Está seguro de que desea eliminar los archivos seleccionados?'),
+                    function() {
+                        selected.forEach(function(item) {
+                            frappe.call({
+                                method: 'tm_app.tm_crm.archivos_pdf.eliminar_archivo_pdf',
+                                args: { archivo_id: item.name },
+                                callback: function(r) {
+                                    if (r.message) {
+                                        frappe.msgprint(__('Archivo eliminado con éxito'));
+                                        listview.refresh(); // Refrescar la lista
+                                    }
+                                },
+                            });
+                        });
+                    },
+                    function() {
+                        frappe.msgprint(__('Proceso cancelado'));
+                    }
+                );
             }
         });
     }
-}
+};
